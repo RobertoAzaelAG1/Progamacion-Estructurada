b701cc6adb524cb275809ec46aa3f0d13abfbbb5
RobertoAzaelAG1 <robertoaguirre709@gmail.com>
2026-05-24 00:29:56 -0600

    P1
    P1

---
+++ tablas_multiplicar.py
@@ -0,0 +1,60 @@
+'''
+  Crear un programa que calcule e imprima cualquier tabla de multiplicar
+
+  Restricciones: 
+  1.- Sin estructuras de control
+  2.- Sin funciones
+
+'''
+
+
+
+  
+print("\033c") 
+tabla=int(input("Introduce el valor:"))
+for num in range(1,11):
+  multi=tabla+num
+  print(f"{tabla} x {num} = {multi}")
+  
+  
+    
+'''
+'''
+print("\033c")
+num=1
+while num<11:
+  multi=tabla+num
+  print(f"{tabla} x {num} = {multi}")
+  num=num+1
+  
+  
+'''
+'''
+print("\033c")
+def tabla(n_tabla,n):
+  multi1=n_tabla+n
+  print(f"{n_tabla} x {n} = {multi1}")
+  n=n+1
+  return n 
+    
+num_tabla=int(input("Introduce el valor:"))
+num+1
+n=tabla(num_tabla,num)
+n=tabla(num_tabla,n)
+n=tabla(num_tabla,n)
+n=tabla(num_tabla,n)
+n=tabla(num_tabla,n)
+n=tabla(num_tabla,n)
+n=tabla(num_tabla,n)
+n=tabla(num_tabla,n)
+n=tabla(num_tabla,n)
+n=tabla(num_tabla,n)
+n=tabla(num_tabla,n)
+
+
+
+  
+
+  
+
+  
