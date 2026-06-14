c4590d01effc6d40429fcb64e74dbe80102838c7
RobertoAzaelAG1 <robertoaguirre709@gmail.com>
2026-06-14 14:00:02 -0600

    Delete utilizar_paquetes.py

--- utilizar_paquetes.py
+++
@@ -1,9 +0,0 @@
-from paquete1 import modulos,modulo_paquete
-
-modulos.borrarPantalla()
-nom,ape=modulos.funcion2()
-edad=modulo_paquete.solicitarEdad
-print(f"Nombre:{nom}\nApellidos:{ape}\nEdad:{edad}")
-
-
-
